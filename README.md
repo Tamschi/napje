eb9a0d2af2a9187d8147b566629701b3f3c465d5
Tamme Schichler <tamme@schichler.dev>
2021-09-12 18:37:08 +0200

    Some initial project setup and implementation

--- README.md
+++ README.md
@@ -1,19 +1,23 @@
-# TODO_CRATE_NAME
+# napje
 
-[![Lib.rs](https://img.shields.io/badge/Lib.rs-*-84f)](https://lib.rs/crates/TODO_CRATE_NAME)
-[![Crates.io](https://img.shields.io/crates/v/TODO_CRATE_NAME)](https://crates.io/crates/TODO_CRATE_NAME)
-[![Docs.rs](https://docs.rs/TODO_CRATE_NAME/badge.svg)](https://docs.rs/TODO_CRATE_NAME)
+[![Lib.rs](https://img.shields.io/badge/Lib.rs-*-84f)](https://lib.rs/crates/napje)
+[![Crates.io](https://img.shields.io/crates/v/napje)](https://crates.io/crates/napje)
+[![Docs.rs](https://docs.rs/napje/badge.svg)](https://docs.rs/napje)
 
 ![Rust 1.51](https://img.shields.io/static/v1?logo=Rust&label=&message=1.51&color=grey)
-[![CI](https://github.com/Tamschi/TODO_CRATE_NAME/workflows/CI/badge.svg?branch=develop)](https://github.com/Tamschi/TODO_CRATE_NAME/actions?query=workflow%3ACI+branch%3Adevelop)
-![Crates.io - License](https://img.shields.io/crates/l/TODO_CRATE_NAME/0.0.1)
+[![CI](https://github.com/Tamschi/napje/workflows/CI/badge.svg?branch=develop)](https://github.com/Tamschi/napje/actions?query=workflow%3ACI+branch%3Adevelop)
+![Crates.io - License](https://img.shields.io/crates/l/napje/0.0.1)
 
-[![GitHub](https://img.shields.io/static/v1?logo=GitHub&label=&message=%20&color=grey)](https://github.com/Tamschi/TODO_CRATE_NAME)
-[![open issues](https://img.shields.io/github/issues-raw/Tamschi/TODO_CRATE_NAME)](https://github.com/Tamschi/TODO_CRATE_NAME/issues)
-[![open pull requests](https://img.shields.io/github/issues-pr-raw/Tamschi/TODO_CRATE_NAME)](https://github.com/Tamschi/TODO_CRATE_NAME/pulls)
-[![good first issues](https://img.shields.io/github/issues-raw/Tamschi/TODO_CRATE_NAME/good%20first%20issue?label=good+first+issues)](https://github.com/Tamschi/TODO_CRATE_NAME/contribute)
+[![GitHub](https://img.shields.io/static/v1?logo=GitHub&label=&message=%20&color=grey)](https://github.com/Tamschi/napje)
+[![open issues](https://img.shields.io/github/issues-raw/Tamschi/napje)](https://github.com/Tamschi/napje/issues)
+[![open pull requests](https://img.shields.io/github/issues-pr-raw/Tamschi/napje)](https://github.com/Tamschi/napje/pulls)
+[![good first issues](https://img.shields.io/github/issues-raw/Tamschi/napje/good%20first%20issue?label=good+first+issues)](https://github.com/Tamschi/napje/contribute)
 
-[![crev reviews](https://web.crev.dev/rust-reviews/badge/crev_count/TODO_CRATE_NAME.svg)](https://web.crev.dev/rust-reviews/crate/TODO_CRATE_NAME/)
+[![crev reviews](https://web.crev.dev/rust-reviews/badge/crev_count/napje.svg)](https://web.crev.dev/rust-reviews/crate/napje/)
+
+> > Een **napje** […] is een structuur die onder andere voorkomt bij de plantenfamilie *Fagaceae*, die de Nederlandse naam "napjesdragersfamilie" heeft.
+>
+> Dutch Wikipedia on cupules.
 
 TODO_README_DESCRIPTION
 
@@ -22,7 +26,7 @@ TODO_README_DESCRIPTION
 Please use [cargo-edit](https://crates.io/crates/cargo-edit) to always add the latest version of this library:
 
 ```cmd
-cargo add TODO_CRATE_NAME
+cargo add napje
 ```
 
 ## Example
@@ -54,7 +58,7 @@ dual licensed as above, without any additional terms or conditions.
 
 ## Versioning
 
-`TODO_CRATE_NAME` strictly follows [Semantic Versioning 2.0.0](https://semver.org/spec/v2.0.0.html) with the following exceptions:
+`napje` strictly follows [Semantic Versioning 2.0.0](https://semver.org/spec/v2.0.0.html) with the following exceptions:
 
 * The minor version will not reset to 0 on major version changes (except for v1).  
 Consider it the global feature level.
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,173 @@
-#![doc(html_root_url = "https://docs.rs/TODO_CRATE_NAME/0.0.1")]
+#![doc(html_root_url = "https://docs.rs/napje/0.0.1")]
 #![warn(clippy::pedantic)]
 #![allow(clippy::semicolon_if_nothing_returned)]
+#![allow(clippy::inline_always)] // `const fn` with added constraints is unstable.
+
+use std::{ops::Deref, pin::Pin};
 
 #[cfg(doctest)]
 pub mod readme {
 	doc_comment::doctest!("../README.md");
+}
+
+/// A pinning wrapper for a collection type `C` that can pin-project to its items while pinned this way.
+///
+/// Unlike when using [`Pin<&C>`](`std::pin::Pin`), this allows the collection itself to stay [`Unpin`].
+///
+/// [`ItemsPin<C>`] acts to [`C: Items<Item = T>`](`Items`) as [`Pin<P>`](`std::pin::Pin`) does to [`P: Deref<Target = T>`](`std::ops::Deref`).
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct ItemsPin<C: ?Sized> {
+	collection: C,
+}
+
+impl<'a, C: Sized> ItemsPin<C>
+where
+	C: Items<'a>,
+	C::Item: Unpin,
+{
+	#[inline(always)]
+	pub fn new(collection: C) -> ItemsPin<C> {
+		Self { collection }
+	}
+
+	/// Unwraps this [`ItemsPin<C>`], returning the underlying collection.
+	///
+	/// This requires that the items inside this [`ItemsPin`] are [`Unpin`],
+	/// so that we can ignore the pinning invariants when unwrapping it.
+	#[inline(always)]
+	pub fn into_inner(items_pin: ItemsPin<C>) -> C {
+		items_pin.collection
+	}
+}
+
+impl<'a, C: Sized> ItemsPin<C>
+where
+	C: Items<'a>,
+{
+	/// Constructs a new [`ItemsPin<P>`] around a collection of items of a type that may or may not implement [`Unpin`].
+	///
+	/// If `collection` contains items of an [`Unpin`] type, [`ItemsPin::new`] should be used instead.
+	///
+	/// # Safety
+	///
+	/// See [`Pin::new_unchecked`].
+	#[inline(always)]
+	pub unsafe fn new_unchecked(collection: C) -> ItemsPin<C> {
+		Self { collection }
+	}
+
+	/// Unwraps this `ItemsPin<C>`, returning the underlying collection.
+	///
+	/// # Safety
+	///
+	/// See [`Pin::into_inner_unchecked`].
+	#[inline(always)]
+	pub unsafe fn into_inner_unchecked(items_pin: ItemsPin<C>) -> C {
+		items_pin.collection
+	}
+}
+
+pub trait Items<'a> {
+	type Item: 'a;
+	type ItemsIter: 'a + Iterator<Item = &'a Self::Item>;
+
+	fn items(&'a self) -> Self::ItemsIter;
+}
+
+pub trait ItemsPinned<'a>: Items<'a> {
+	type ItemsPinnedIter: 'a + Iterator<Item = Pin<&'a Self::Item>>;
+
+	fn items_pinned(&'a self) -> Self::ItemsPinnedIter;
+}
+
+pub trait ItemsMut<'a>: Items<'a> {
+	type ItemsMutIter: 'a + Iterator<Item = &'a mut Self::Item>;
+
+	fn items_mut(&'a mut self) -> Self::ItemsMutIter;
+}
+
+pub trait ItemsPinnedMut<'a>: ItemsPinned<'a> + ItemsMut<'a> {
+	type ItemsPinnedMutIter: 'a + Iterator<Item = Pin<&'a mut Self::Item>>;
+
+	fn items_pinned_mut(&'a mut self) -> Self::ItemsPinnedMutIter;
+}
+
+pub struct PinIter<Iter> {
+	iter: Iter,
+}
+
+impl<Iter> PinIter<Iter>
+where
+	Iter: Iterator,
+	Iter::Item: Deref,
+{
+	pub fn new(iter: Iter) -> Self
+	where
+		<Iter::Item as Deref>::Target: Unpin,
+	{
+		Self { iter }
+	}
+
+	pub unsafe fn new_unchecked(iter: Iter) -> Self {
+		Self { iter }
+	}
+}
+
+impl<Iter> Iterator for PinIter<Iter>
+where
+	Iter: Iterator,
+	Iter::Item: Deref,
+{
+	type Item = Pin<Iter::Item>;
+
+	fn next(&mut self) -> Option<Self::Item> {
+		self.iter
+			.next()
+			.map(|item| unsafe { Pin::new_unchecked(item) })
+	}
+}
+
+impl<'a, C: ?Sized> Items<'a> for ItemsPin<C>
+where
+	C: Items<'a>,
+{
+	type Item = C::Item;
+	type ItemsIter = C::ItemsIter;
+
+	fn items(&'a self) -> Self::ItemsIter {
+		self.collection.items()
+	}
+}
+
+impl<'a, C: ?Sized> ItemsPinned<'a> for ItemsPin<C>
+where
+	C: Items<'a>,
+{
+	type ItemsPinnedIter = PinIter<C::ItemsIter>;
+
+	fn items_pinned(&'a self) -> Self::ItemsPinnedIter {
+		unsafe { PinIter::new_unchecked(self.collection.items()) }
+	}
+}
+
+impl<'a, C: ?Sized> ItemsMut<'a> for ItemsPin<C>
+where
+	C: ItemsMut<'a>,
+{
+	type ItemsMutIter = C::ItemsMutIter;
+
+	fn items_mut(&'a mut self) -> Self::ItemsMutIter {
+		self.collection.items_mut()
+	}
+}
+
+impl<'a, C: ?Sized> ItemsPinnedMut<'a> for ItemsPin<C>
+where
+	C: ItemsMut<'a>,
+{
+	type ItemsPinnedMutIter = PinIter<C::ItemsMutIter>;
+
+	fn items_pinned_mut(&'a mut self) -> Self::ItemsPinnedMutIter {
+		unsafe { PinIter::new_unchecked(self.collection.items_mut()) }
+	}
 }
